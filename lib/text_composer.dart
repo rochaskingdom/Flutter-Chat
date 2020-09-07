3d8c8c6aab7f2e6b0a59977da73be342584b2697
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-09-07 16:35:37 -0300

    Enviando o Texto para o Firebase

--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
 class TextComposer extends StatefulWidget {
+
+  TextComposer(this.sendMessage);
+
+  Function(String) sendMessage;
+
   @override
   _TextComposerState createState() => _TextComposerState();
 }
 
 class _TextComposerState extends State<TextComposer> {
+
+  final TextEditingController _controller = TextEditingController();
+
   bool _isComposing = false;
+
+  void _reset() {
+    _controller.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +35,7 @@ class _TextComposerState extends State<TextComposer> {
           ),
           Expanded(
             child: TextField(
+              controller: _controller,
               decoration:
                   InputDecoration.collapsed(hintText: 'Enviar uma Mensagem'),
               onChanged: (text) {
@@ -27,12 +43,18 @@ class _TextComposerState extends State<TextComposer> {
                   _isComposing = text.isNotEmpty;
                 });
               },
-              onSubmitted: (text) {},
+              onSubmitted: (text) {
+                widget.sendMessage(text);
+                _reset();
+              },
             ),
           ),
           IconButton(
-            icon: Icon(Icons.send, color: Colors.blue),
-            onPressed: _isComposing ? () {} : null,
+            icon: Icon(Icons.send),
+            onPressed: _isComposing ? () {
+              widget.sendMessage(_controller.text);
+              _reset();
+            } : null,
           )
         ],
       ),
